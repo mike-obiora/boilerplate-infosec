9186f1d2e6359233a9ba06bf46f27c8fc5cedd97
mike-obiora <micheal.obiora@outlook.com>
2024-10-08 14:45:27 +0100

    helmet update

--- myApp.js
+++ myApp.js
@@ -2,49 +2,7 @@ const express = require('express');
 const helmet = require('helmet');
 const app = express();
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+const PORT = process.env.port || 3000;
 
 
 
